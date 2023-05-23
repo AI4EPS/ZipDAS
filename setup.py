876b508d32a296c8dfbfc7c9bf7ebea77299e8cd
Weiqiang <zhuwq0@gmail.com>
2023-05-23 16:59:52 -0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ZipDAS",
-    version="0.1.0",
+    version="0.1.1",
     long_description="DAS Data Compression",
     long_description_content_type="text/markdown",
     packages=["zipdas"],
